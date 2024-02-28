00d95df3170e22d8bb72aa8faaeef9e107513c18
Rox Niederk <rox.niederkorn@gmail.com>
2024-02-28 12:31:00 -0600

    added comments, added JS to dynamically render dice corresponding to the randomly generated number. diplayed which player won or if it was a draw

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-var randomNumber1 = 1 + Math.floor(Math.random() * 6);
+// Generate a random number from 1 to 6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-console.log(randomNumber1);
+// Get the dice image element- create new variable to select image element
+var diceImage1 = document.querySelector('.img1');
+var diceImage2 = document.querySelector('.img2');
+
+// Set the source of the image based on the random number
+diceImage1.setAttribute('src', './images/dice' + randomNumber1 + '.png');
+diceImage2.setAttribute('src', './images/dice' + randomNumber2 + '.png');
+
+
+// Display which player won, or that it was a draw
+var result = document.querySelector('h1');
+
+if (randomNumber1 === randomNumber2) {
+    result.innerHTML="Draw!";
+}
+else if (randomNumber1 >  randomNumber2) {
+    result.innerHTML="🚩 Player 1 Wins!";
+}
+else {
+    result.innerHTML="Player 2 Wins! 🚩"
+}
